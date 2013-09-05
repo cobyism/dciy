29810b11739cb42235be38493a21b27f100ad6d6
Coby Chapple <coby@cobychapple.com>
2013-09-05 11:22:30 +0100

    steal stuff from integrity, hack together a working build runner

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ActiveRecord::Base
   has_many :builds
+
+  def workspace_path
+    Rails.root.join("workspace", "project-#{self.id}-#{self.repo.gsub(/\//, '-')}")
+  end
+
+  def repo_uri
+    "https://github.com/#{repo}"
+  end
 end
