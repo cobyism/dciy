36bfc0ed32abc5d7ec1e79dd6d732813a2e507b7
Ash Wilson <smashwilson@gmail.com>
2013-09-14 12:07:52 -0400

    Move #ci_command determination to the Project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,29 @@ class Project < ActiveRecord::Base
     Rails.root.join("workspace", "project-#{self.id}-#{self.repo.gsub(/\//, '-')}")
   end
 
+  def ci_command
+    case
+    when has_file?('dciy.toml')
+      instructions = TOML.load_file(workspace_file 'dciy.toml')
+      instructions['dciy']['commands']['cibuild']
+    when has_file?('script/cibuild')
+      'script/cibuild'
+    else
+      raise CantFindBuildFile.new
+    end
+  end
+
   def repo_uri
     "https://github.com/#{repo}"
   end
+
+  def has_file? filename
+    File.exist?(workspace_file filename)
+  end
+
+  def workspace_file filename
+    File.join(workspace_path, filename)
+  end
 end
+
+class CantFindBuildFile < RuntimeError ; end
