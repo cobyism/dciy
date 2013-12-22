d25d5f408a114841e9b6b3dc812295492b6d4d30
Joe Wilm <jdwilm@gmail.com>
2013-12-22 12:06:55 -0800

    Remove index action from Projects
    - Scrub view/js builder
    - Remove index action
    - Update router
    - Remove link to projectspath

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,6 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   before_action :set_hosts, only: [:new, :edit]
 
-  # GET /projects
-  # GET /projects.json
-  def index
-    @projects = Project.all
-  end
-
   # GET /projects/1
   # GET /projects/1.json
   def show
--- app/views/projects/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@projects) do |project|
-  json.extract! project, :repo
-  json.url project_url(project, format: :json)
-end
