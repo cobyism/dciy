8cf28108d4a1ad905e9df1503e9675becb39cd22
Joe Wilm <jdwilm@gmail.com>
2013-12-22 12:12:44 -0800

    Add destroy link to edit project
    - Destroy button now on project edit view
    - Redirect to root path after project destroy
    - Add intelligent go back button to project form partial

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -50,7 +50,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to root_path }
       format.json { head :no_content }
     end
   end
