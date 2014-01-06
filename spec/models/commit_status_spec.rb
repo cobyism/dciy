07b33fe3220e924b62bc945e413b23a5d469eee7
Ash Wilson <smashwilson@gmail.com>
2014-01-05 22:00:13 -0500

    Refactor the commit_status_spec.
    Remove some duplication.

--- spec/models/commit_status_spec.rb
+++ spec/models/commit_status_spec.rb
@@ -5,6 +5,15 @@ describe CommitStatus do
   let(:user) { "fakey" }
   let(:token) { "ABCDEF123" }
 
+  def expect_post build, endpoint, payload
+    repo_name = build.project.repo
+    sha = build.sha
+    expect(RestClient::Resource).to receive(:new).with(endpoint, user, token).and_return(resource)
+
+    dict = { content_type: :json, accept: :json }
+    expect(resource).to receive(:post).with(payload.to_json, dict).and_return(nil)
+  end
+
   context "with a username and token" do
     before do
       ENV["GITHUB_API_USER"], ENV["GITHUB_API_TOKEN"] = user, token
@@ -12,18 +21,12 @@ describe CommitStatus do
 
     it 'sets a pending status' do
       build = builds(:one)
-      repo_name = build.project.repo
-      sha       = build.sha
-      endpoint = "https://api.github.com/repos/#{repo_name}/statuses/#{sha}"
-
-      # Here's where we get RestClient::Resource to return the fake "resource" we're injecting...
-      expect(RestClient::Resource).to receive(:new).with(endpoint, user, token).and_return(resource)
-
-      commit_json = '{"state":"pending","description":"Build pending.","target_url":"http://localhost:6161/builds/1"}'
-      dict = { content_type: :json, accept: :json }
-
-      # ... and here's where we tell it how it should be called:
-      expect(resource).to receive(:post).with(commit_json, dict).and_return(nil)
+      endpoint = "https://api.github.com/repos/#{build.project.repo}/statuses/#{build.sha}"
+      expect_post build, endpoint, {
+        state: "pending",
+        description: "Build pending.",
+        target_url: "http://localhost:6161/builds/#{build.id}"
+      }
 
       CommitStatus.mark build.id, :pending
       # If +resource+ doesn't get sent :post, or gets the wrong arguments, boom! spec failure.
@@ -31,16 +34,12 @@ describe CommitStatus do
 
     it "uses the project's GitHub API endpoint" do
       build = builds(:two)
-      repo_name = build.project.repo
-      sha = build.sha
-      endpoint = "https://github.internal.com/api/v3/repos/#{repo_name}/statuses/#{sha}"
-
-      expect(RestClient::Resource).to receive(:new).with(endpoint, user, token).and_return(resource)
-
-      commit_json = '{"state":"pending","description":"Build pending.","target_url":"http://localhost:6161/builds/2"}'
-      dict = { content_type: :json, accept: :json }
-
-      expect(resource).to receive(:post).with(commit_json, dict)
+      endpoint = "https://github.internal.com/api/v3/repos/#{build.project.repo}/statuses/#{build.sha}"
+      expect_post build, endpoint, {
+        state: "pending",
+        description: "Build pending.",
+        target_url: "http://localhost:6161/builds/#{build.id}"
+      }
 
       CommitStatus.mark build.id, :pending
     end
