c235a1b3d96599691d118933b3767b10bb526323
Ash Wilson <ash.wilson@rackspace.com>
2014-01-07 09:52:31 -0500

    Specs for deriving GitHub host choices.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -33,10 +33,48 @@ describe ProjectsController do
     expect(response).to be_success
   end
 
-  it "should get edit" do
+  it "should edit project" do
     get :edit, id: project
     expect(assigns :project).to eq(project)
     expect(response).to be_success
+  end
+
+  describe "github host choices" do
+    let(:other_host) { projects(:two) }
+
+    context "without an ENTERPRISE_HOST" do
+      before do
+        allow(subject).to receive(:enterprise_host).and_return(nil)
+      end
+
+      it "shouldn't offer a choice of github host" do
+        get :new, id: project
+        expect(assigns :choice).to eq([])
+      end
+
+      it "offers a choice if the project already specifies a different host" do
+        get :edit, id: other_host
+        expect(assigns :choice).to eq(['github.com', other_host.github_host])
+      end
+    end
+
+    context "with an ENTERPRISE_HOST" do
+      before do
+        allow(subject).to receive(:enterprise_host).and_return("github.starship-enterprise.com")
+      end
+
+      it "should offer a choice of github hosts" do
+        get :new, id: project
+        expect(assigns :choice).to eq(%w{github.com github.starship-enterprise.com})
+      end
+
+      it "offers the original host as a choice on existing projects" do
+        get :edit, id: other_host
+        expect(assigns :choice).to eq(['github.com',
+          'github.starship-enterprise.com',
+          other_host.github_host])
+      end
+    end
   end
 
   it "should update project" do
