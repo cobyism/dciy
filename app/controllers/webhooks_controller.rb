88ea1c572f337f373705f037294a1e87c5491340
Ash Wilson <ash.wilson@rackspace.com>
2014-01-17 15:58:38 -0500

    Parse GitHub POST hook output and create Builds.

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,5 +2,22 @@ class WebhooksController < ApplicationController
 
   # POST /webhooks/receive
   def receive
+    push = JSON.parse(params[:payload])
+
+    pname = push.fetch('repository', {})['name']
+    p = Project.where(repo: pname).take
+
+    unless p
+      head :not_found
+      return
+    end
+
+    bname = push['ref']
+    bname.gsub!(%r{^refs/heads/}, '') if bname
+    b = p.builds.create! branch: bname
+
+    BuildWorker.perform_async(b.id)
+
+    head :created
   end
 end
--- spec/controllers/webhooks_controller_spec.rb
+++ spec/controllers/webhooks_controller_spec.rb
@@ -2,10 +2,38 @@ require 'spec_helper'
 
 describe WebhooksController do
 
-  describe "GET 'receive'" do
-    it "returns http success" do
-      get 'receive'
-      response.should be_success
+  describe "POST 'receive'" do
+    let!(:project) { Project.create! repo: 'cobyism/dciy' }
+
+    it "creates a new build for that ref, on that project" do
+      expect(BuildWorker).to receive(:perform_async)
+
+      # Reference:
+      # https://help.github.com/articles/post-receive-hooks#the-payload
+      expect do
+        post :receive, payload: {
+          ref: 'refs/heads/master',
+          repository: {
+            name: 'cobyism/dciy'
+          }
+        }.to_json
+      end.to change { project.builds.count }.by(1)
+
+      expect(response).to be_success
+      expect(project.builds).to have(1).item
+      b = project.builds[0]
+      expect(b.branch).to eq("master")
+    end
+
+    it "returns a 404 on unknown projects" do
+      post :receive, payload: {
+        ref: 'refs/heads/master',
+        repository: {
+          name: 'attacker/exploitz'
+        }
+      }.to_json
+
+      expect(response).to be_not_found
     end
   end
 
