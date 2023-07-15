b116cc7aeaa119dd4a16935793f0efdc24f6e334
keitamori <keitamori@gmail.com>
2023-07-15 16:09:35 +0200

    Prise en compte es6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ DCIY::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true) 
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
